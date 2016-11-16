2c7e9dbc5e9b49e9c3d6d0b778ee557efb049bd1
ZoeLeBlanc <zoe.leblanc@vanderbilt.edu>
2016-11-15 19:33:25 -0600

    got inputs and search

--- javascripts/app.js
+++ javascripts/app.js
@@ -6,17 +6,34 @@ app.controller("NavCtrl", function($scope){
 	$scope.navItems = [{name:"Logout"},{name:"All Items"},{name:"New Item"}];
 });
 app.controller("TodoCtrl", function($scope){
+	$scope.newTask = {};
 	$scope.welcome = "hey there";
 	$scope.title = "Welcome to your task list Zoe";
 	$scope.showListView = true;
 	$scope.newItem = function() {
 		console.log("one item");
 		$scope.showListView = false;
-	}
+	};
 	$scope.allItems = function(){
 		console.log("all items");
 		$scope.showListView = true;
-	}
+	};
+	// $scope.addNewItem = function(){
+	// 	$scope.items.$add({
+	// 		id: 
+	// 		task: $scope.newTask.task,
+	// 		isCompleted: false,
+	// 		assignedTo: $scope.newTask.assignedTo
+	// 	});
+	// }
+	$scope.addNewItem = function(){
+		$scope.newTask.isCompleted = false;
+		$scope.newTask.id = $scope.items.length;
+		console.log("newTask in function, ", $scope.newTask);
+		$scope.items.push($scope.newTask);
+		$scope.newTask = {};
+		$scope.showListView = true;
+	};
 	$scope.items = [
     { 
     	id: 0,
